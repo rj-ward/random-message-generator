d544efce3b80884fa918177140f9605208d88757
Remy Ward <remyward@protonmail.com>
2024-02-25 09:38:02 -0600

    Add import statement to index.js

--- card-library.js
+++ card-library.js
@@ -1,111 +1,111 @@
-let fool = {
+export let fool = {
     name: 'The Fool',
     keywords: 'Beginning, Spontaneity, Faith, Apparent Folly'
 };
 
-let magician = {
+export let magician = {
     name: 'The Magician',
     keywords: 'Action, Conscious Awareness, Concentration, Power'
 };
 
-let priestess = {
+export let priestess = {
     name: 'The High Priestess',
     keywords: 'NonAction, Unconscious Awareness, Potential, Mystery'
 };
 
-let empress = {
+export let empress = {
     name: 'The Empress',
     keywords: 'Motherhood, Abundance, Senses, Nature'
 };
 
-let emperor = {
+export let emperor = {
     name: 'The Emperor',
     keywords: 'Fatherhood, Structure, Authority, Regulation'
 };
 
-let hierophant = {
+export let hierophant = {
     name: 'The Heirophant',
     keywords: 'Education, Belief Systems, Conformity, Group Identification'
 };
 
-let lovers = {
+export let lovers = {
     name: 'The Lovers',
     keywords: 'Relationship, Sexuality, Personal Beliefs Values'
 };
 
-let chariot = {
+export let chariot = {
     name: 'The Chariot',
     keywords: 'Victory, Will, Self-Assertion, Hard Control'
 };
 
-let strength = {
+export let strength = {
     name: 'Strength',
     keywords: 'Strength, Patience, Compassion, Soft Control'
 };
 
-let hermit = {
+export let hermit = {
     name: 'The Hermit',
     keywords: 'Introspection, Searching, Guidance, Solitude'
 };
 
-let wheel = {
+export let wheel = {
     name: 'The Wheel of Fortune',
     keywords: 'Destiny, Turning Point, Movement, Personal Vision'
 };
 
-let justice = {
+export let justice = {
     name: 'Justice',
     keywords: 'Justice, Responsibility, Decision, Cause and Effect'
 };
 
-let hanged = {
+export let hanged = {
     name: 'The Hanged Man',
     keywords: 'Letting Go, Reversal, Suspension, Sacrifice'
 };
 
-let death = {
+export let death = {
     name: 'Death',
     keywords: 'Ending, Transition, Elimination, Inexorable Forces'
 };
 
-let temperance = {
+export let temperance = {
     name: 'Temperance',
     keywords: 'Temperance, Balance, Health, Combination'
 };
 
-let devil = {
+export let devil = {
     name: 'The Devil',
     keywords: 'Bondage, Materialism, Ignorance, Hopelessness'
 };
 
-let tower = {
+export let tower = {
     name: 'The Tower',
     keywords: 'Sudden Change, Release, Downfall, Revelation'
 };
 
-let star = {
+export let star = {
     name: 'The Star',
     keywords: 'Hope, Inspiration, Generosity, Serenity'
 };
 
-let moon = {
+export let moon = {
     name: 'The Moon',
     keywords: 'Fear, Illusion, Imagination, Bewilderment'
 };
 
-let sun = {
+export let sun = {
     name: 'The Sun',
     keywords: 'Enlightenment, Greatness, Vitality, Assurance'
 };
 
-let judgement = {
+export let judgement = {
     name: 'Judgement',
     keywords: 'Judgment, Rebirth, Inner Calling, Absolution'
 };
 
-let world = {
+export let world = {
     name: 'The World',
     keywords: 'Integration, Accomplishment, Involvement, Fulfillment'
 };
 
-let deck = [fool, magician, priestess, empress, emperor, hierophant, lovers, chariot, strength, hermit, wheel, justice, hanged, death, temperance, devil, tower, star, moon, sun, judgement, world];
+export let deck = [fool, magician, priestess, empress, emperor, hierophant, lovers, chariot, strength, hermit, wheel, justice, hanged, death, temperance, devil, tower, star, moon, sun, judgement, world];
